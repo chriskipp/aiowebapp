cc9f9b97e63f0d192a2db665d638f868fec86813
chriskipp <christopher.kipp@web.de>
2021-01-02 13:43:47 +0100

    Added map to tools

--- app/app/handlers/tools.py
+++ app/app/handlers/tools.py
@@ -11,7 +11,15 @@ class ToolsHandler(BaseHandler):
             context=await self.get_context(request, {}),
         )
 
+    async def map(self, request):
+        return aiohttp_jinja2.render_template(
+            "leaflet.html",
+            request,
+            context=await self.get_context(request, {}),
+        )
+
     def configure(self, app):
 
         router = app.router
         router.add_route("GET", "/editor", self.editor, name="editor")
+        router.add_route("GET", "/map", self.map, name="map")
